aebbb90661f3645175525bc3c0135e99e4ec6c96
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-13 14:15:47 -0600

    Update intro-home.php

--- inc/intro-home.php
+++ inc/intro-home.php
@@ -9,13 +9,17 @@
 				<?php the_field('intro_right_content'); ?>
 			</div>
 		</div>
-		<?php if( get_field('additional_copy') ): ?>
-		<div class="row additional-home-copy">
+	</div>
+</div>
+<?php if( get_field('additional_copy') ): ?>
+<div class="container-fluid container-padding additional-home-copy">
+	<div class="container">
+		<div class="row">
 			<div class="col-sm-12">
 				<?php the_field('additional_copy')?>
 			</div>
 		</div>
-		<?php endif; ?>
 	</div>
 </div>
+<?php endif; ?>
 <?php endif; ?>
