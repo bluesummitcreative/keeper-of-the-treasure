1d4991ef0e0dc767d14f8f1150c2f6036ed058c4
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-13 15:37:58 -0600

    Update site-wide-cta.php

--- inc/site-wide-cta.php
+++ inc/site-wide-cta.php
@@ -8,4 +8,18 @@
 		</div>
 	</div>
 </div>
+
+<div class="container-fluid container-padding site-wide-cta text-center">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h3>Find the Pause</h3>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur justo nulla, facilisis non molestie ut.
+
+				<a class="btn btn-primary" href="https://catspawcushions.com/shop/">Buy the Journal</a>
+			</div>
+		</div>
+	</div>
+</div>
+
 <?php endif; ?>
