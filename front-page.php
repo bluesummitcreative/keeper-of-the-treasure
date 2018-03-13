fb8ec97c9ccb8a31280291239772fc0c6abf92f3
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-13 14:38:38 -0600

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -4,6 +4,5 @@
 	//get_template_part('inc/slider-home');
 	get_template_part('inc/intro-home');
 	get_template_part('inc/funnels-home');
-	get_template_part('inc/site-wide-cta');
 	get_footer(); 
 ?>
