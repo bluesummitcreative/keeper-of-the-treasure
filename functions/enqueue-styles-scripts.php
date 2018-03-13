1eb5f5566afe0ec621c48dd59924e635f1e98b49
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-13 13:13:56 -0600

    Update enqueue-styles-scripts.php

--- functions/enqueue-styles-scripts.php
+++ functions/enqueue-styles-scripts.php
@@ -3,6 +3,7 @@
 	function bsc_enqueue_styles_scripts() {
 		wp_enqueue_style( 'bootstrap.min', '//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css'); // Bootstrap CSS
 		wp_enqueue_style('font-awesome', '//use.fontawesome.com/releases/v5.0.1/css/all.css'); // Font Awesome
+		wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Lato'); // Google Fonts
 		wp_enqueue_script( 'html5shiv.min', '//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js' ); //HTML5 Shiv
 		wp_script_add_data( 'html5shiv.min', 'conditional', 'lt IE 9' ); //Get HTML Shiv if less than IE9
 		wp_enqueue_script( 'respond.min', '//oss.maxcdn.com/respond/1.4.2/respond.min.js' ); //Respond JS
