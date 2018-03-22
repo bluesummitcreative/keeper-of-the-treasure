0a5e4193e219e3697f3f75947214a93de6a6e1f3
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-21 20:57:19 -0600

    Update header.php

--- header.php
+++ header.php
@@ -4,6 +4,16 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Google Analytics -->
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-116216216-1', {'allowAnchor': true});
+			ga('send', 'pageview', { 'page': location.pathname + location.search + location.hash});
+		</script>
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
